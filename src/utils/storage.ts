0d205505cbc27d210c58eb518b510d84f07ea33e
Xiaobing Zhu <Hello16369@163.com>
2024-09-09 20:52:52 +0800

    feat(storage): integrate Async Storage for react-native
    Integrate the latest version of Async Storage into the react-native project to facilitate
    persistent key-value storage. This includes the addition of the relevant dependency in
    the package.json and yarn.lock files, as well as the implementation of utility functions
    for saving, fetching, and removing items from the storage in the src/utils/storage.ts file.

---
+++ src/utils/storage.ts
@@ -0,0 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function save(key: string, value: Record<string, any> | string | number | any[]) {
+  try {
+    return await AsyncStorage.setItem(key, JSON.stringify(value))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function fetch(key: string) {
+  try {
+    const getValue = await AsyncStorage.getItem(key)
+
+    return getValue ? JSON.parse(getValue) : null
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
+export async function remove(key: string) {
+  try {
+    return await AsyncStorage.removeItem(key)
+  } catch (error) {
+    console.error(error)
+  }
+}
